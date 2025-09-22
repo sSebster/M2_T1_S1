1f9f91e6ad069fe883c7c50e0ac2882aa42b52f8
sSebster <sknet@live.fr>
2025-09-22 11:47:44 +0200

    initial commit

---
+++ Source/M2_T1_S1/M2_T1_S1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "M2_T1_S1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, M2_T1_S1, "M2_T1_S1" );
