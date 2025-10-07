624b846dd8573a8a9a13fe3c8d174365cb611a25
SneekeeStache <kyle.prudhomme@proton.me>
2025-10-07 19:59:31 +0200

    debut init task

---
+++ Source/M2_T1_S1/Public/BehaviorTree/Task/SpawnProjectilTask.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SpawnProjectilTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class M2_T1_S1_API USpawnProjectilTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
