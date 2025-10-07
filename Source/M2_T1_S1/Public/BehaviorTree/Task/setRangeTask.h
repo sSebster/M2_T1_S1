94e133fa7b68370d27615cf231e9e9400bff73dc
SneekeeStache <kyle.prudhomme@proton.me>
2025-10-07 20:38:59 +0200

    init tasks kyle

---
+++ Source/M2_T1_S1/Public/BehaviorTree/Task/setRangeTask.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "setRangeTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class M2_T1_S1_API UsetRangeTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
